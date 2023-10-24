b43f1485061455466c54830da609713f8bff6c4c
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-10-24 12:34:56 -0300

    Update pdf_txt_url.py

--- Protótipos/Conversor_PDF_TXT/pdf_txt_url.py
+++ Protótipos/Conversor_PDF_TXT/pdf_txt_url.py
@@ -10,22 +10,6 @@ remote_file = urlopen(Request(url)).read()
 memory_file = io.BytesIO(remote_file)
 pdf = PdfFileReader(memory_file)
 
-# Gerando um txt sem baixar o pdf no seu computador
-with open('amem2.txt', 'w', encoding="utf-8") as f:
-    for page_num in range(pdf.numPages):
-        pageObj = pdf.getPage(page_num)
-
-        try:
-            txt = pageObj.extractText()
-            
-        except:
-            pass
-        else:
-            f.write('page{0}\n0'.format(page_num+1))
-            f.write(''.center(100, '-'))
-            f.write(txt)
-    f.close()
-
 # Gerando arquivo .json sem baixa o pdf no seu computador
 with open('amem2.json', 'w', encoding="utf-8") as d:
     for page_num in range(pdf.numPages):
@@ -33,12 +17,12 @@ with open('amem2.json', 'w', encoding="utf-8") as d:
         pageObj = pdf.getPage(page_num)
 
         try:
-            txt = pageObj.extractText()
+            json = pageObj.extractText()
             print(''.center(100,'-'))
         except:
             pass
         else:
             d.write('page{0}\n0'.format(page_num+1))
             d.write(''.center(100, '-'))
-            d.write(txt)
+            d.write(json)
     d.close()

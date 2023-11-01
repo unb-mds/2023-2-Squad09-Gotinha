17b40d0df3c9a3d2d55b881ccd2fcd2348e49945
Luiz Gustavo <lalmeida727.lg@gmail.com>
2023-11-01 03:56:49 -0300

    Update pdf_txt.py

--- Protótipos/Extrator_2/pdf_txt.py
+++ Protótipos/Extrator_2/pdf_txt.py
@@ -1,38 +1,39 @@
 from urllib.request import Request, urlopen
 from PyPDF2 import PdfFileReader
 import io
+import requests
 import json
 import os
 
-output_directory = 'c:/Users/Usuário/OneDrive/Área de Trabalho/Extrator Atualizado/Diários em TXT'
+output_directory = 'c:/Users/Usuário/OneDrive/Área de Trabalho/2023-2-Squad09-Gotinha/Prótotipos/Extrator_2/Diários em TXT'
 
-local_json_file = 'c:/Users/Usuário/OneDrive/Área de Trabalho/Extrator Atualizado/versao_reduzida.json'
+url = 'https://raw.githubusercontent.com/unb-mds/2023-2-Squad09-Gotinha/main/Protótipos/Extrator_2/versao_reduzida.json'
 
-with open(local_json_file, 'r', encoding='utf-8') as json_file:
-    json_data = json.load(json_file)
+response = requests.get(url)
 
-urls_json = json_data
+if response.status_code == 200:
+    json_data = response.json()
 
-for item in urls_json:
-    url = item['URL']
+    for item in json_data:
+        url = item['URL']
 
-    remote_file = urlopen(Request(url)).read()
-    memory_file = io.BytesIO(remote_file)
-    pdf = PdfFileReader(memory_file, strict=False)
+        remote_file = requests.get(url).content 
+        memory_file = io.BytesIO(remote_file)
+        pdf = PdfFileReader(memory_file, strict=False)
 
-    for page_num in range(pdf.numPages):
-        pageObj = pdf.getPage(page_num)
+        for page_num in range(pdf.numPages):
+            pageObj = pdf.getPage(page_num)
 
-        try:
-            txt = pageObj.extractText()
-                
-        except:
-            pass
-                
-        else:
-            file_name = f"{item['Edicao']}.txt"                    
-            file_path = os.path.join(output_directory, file_name)
+            try:
+                txt = pageObj.extractText()
                     
-            with open(file_path, 'w', encoding="utf-8") as f:
-                f.write(txt)
+            except:
+                pass
+                    
+            else:
+                file_name = f"{item['Edicao']}.txt"                    
+                file_path = os.path.join(output_directory, file_name)
                         
+                with open(file_path, 'w', encoding="utf-8") as f:
+                    f.write(txt)
+                            

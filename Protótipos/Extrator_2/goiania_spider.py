c42f023ac4125266d1b74bbb8452f8372ed16c0e
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-11-03 19:32:15 -0300

    Update goiania_spider.py

--- Protótipos/Extrator_2/goiania_spider.py
+++ Protótipos/Extrator_2/goiania_spider.py
@@ -1,54 +1,62 @@
 import scrapy
 import datetime
 from urllib.parse import urljoin
-import subprocess
 import re
-import json
 
 
 class GoianiaSpider(scrapy.Spider):
+    # Identificador do território
     TERRITORY_ID = '5208707'
+
+    # Nome do spider
     name = 'goiania_spider'
+
+    # Domínio base
     alloy_domain = 'https://goiania.go.gov.br'
+
+    # URL de início da raspagem
     start_url = ['https://www.goiania.go.gov.br/casa-civil/diario-oficial/']
+
+    # Data de início
     start_date = datetime.date(2023, 1, 1)
 
+    # Método para iniciar as requisições
     def start_requests(self):
         year = 2023
+        # Cria uma requisição para o URL com base no ano
         yield scrapy.Request(
             f"http://www.goiania.go.gov.br/shtml//portal/casacivil/lista_diarios.asp?ano={year}"
         )
 
+    # Método para processar as respostas das requisições
     def parse(self, response):
         editions = response.xpath('*//a[@href]')
+        data_list = []  # Lista para armazenar os dados
 
         for edition in editions[:6]:
             e_info = edition.xpath('./text()').get()
-
             num = re.search(r'Edi\u00e7\u00e3o  n\u00ba (\d+)', e_info).group(1)
-
             date = re.search(r'de (\d+ [^\d]+ \d+)', e_info).group(1)
-
             url = edition.xpath('./@href').get()
             full_url = urljoin(f"{self.alloy_domain}", url)
-            
+
             sup = re.search(r'-\s+(.+)$', e_info)
             if sup:
                 sup = "true"
             else:
                 sup = "false"
 
-            yield {
+            data_list.append({
                 'Edicao': num,
                 'Data': date,
                 'URL': full_url,
                 'Suplemento': sup
-            }
+            })
+    
+        return data_list
+        
 
 
 """            
-# Define o comando que você deseja executar no terminal pela IDE #
-comando = "scrapy runspider goiania_api.py -o teste1.json"
-resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE)
-print(resultado.stdout.decode())
+execute o run.py para rodar o goiania_spider.py e o pdf_json.py
 """

f74013292346636b6f11a992252ba8526dd8c629
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-11-21 15:18:19 -0300

    Update run.py
    atualização do código

--- Protótipos/Extrator_V2/run.py
+++ Protótipos/Extrator_V2/run.py
@@ -6,7 +6,7 @@ from filtro import extrair_trechos
 
 # Executa o spider e obtém os diários em .json
 # Lembre-se de navegar pelo terminal até o diretório onde está localizado o arquivo goiania_spider.py
-comando = "scrapy runspider goiania_spider.py -o edicoes_recentes.json"
+comando = "scrapy runspider /Users/2023-2-Squad09-Gotinha/Protótipos/Extrator_V2/goiania_spider.py -o edicoes_recentes.json"
 resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE)
 
 # Obtém o caminho do diretório atual
@@ -29,7 +29,7 @@ url_Json(json_file_path)
 print("Execução da função url_Json foi bem-sucedida!")
 
 # Pasta contendo os arquivos JSON
-pasta_diarios = 'Diários em json'
+pasta_diarios = os.path.join(script_dir, 'Diários em json')
 
 # Lista para armazenar os trechos filtrados
 trechos_filtrados = []
@@ -54,7 +54,10 @@ json_str = json_str.replace('[', '[\n ')
 # Adiciona uma quebra de linha no final
 json_str = json_str.replace(']', '\n]')
 
+# Salva os trechos filtrados em um novo arquivo JSON no mesmo diretório do script
+filtered_file_path = os.path.join(script_dir, 'trechos_filtrados.json')
+
 # Salva os trechos filtrados em um novo arquivo JSON
-with open('trechos_filtrados.json', 'w', encoding='utf-8') as output_file:
+with open(filtered_file_path, 'w', encoding='utf-8') as output_file:
     output_file.write(json_str)
 print("Execução do filtro bem-sucedida! O arquivo 'trechos_filtrados.json' foi gerado")

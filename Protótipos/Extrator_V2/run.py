945fc0f1eaf8f761082ba8771380f3faca1fb6e1
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-12-03 01:28:17 -0300

    Update run.py

--- Protótipos/Extrator_V2/run.py
+++ Protótipos/Extrator_V2/run.py
@@ -3,33 +3,38 @@ import json
 import os
 from pdf_json import url_Json
 from filtro import extrair_trechos
+from goiania_spider import GoianiaSpider
 
 # Executa o spider e obtém os diários em .json
 # Lembre-se de navegar pelo terminal até o diretório onde está localizado o arquivo goiania_spider.py
-comando = "scrapy runspider /2023-2-Squad09-Gotinha/Protótipos/Extrator_V2/goiania_spider.py -o edicoes_2022.json"
-resultado = subprocess.run(comando, shell=True, stdout=subprocess.PIPE)
+
+# Criando uma instância do spider
+spider_instance = GoianiaSpider()
+
+# Acessando a variável 'year' do spider
+year = spider_instance.start_requests().__next__().url.split('=')[-1]
 
 # Obtém o caminho do diretório atual
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Define o caminho completo para o arquivo JSON
-output_file_path = os.path.join(script_dir, "Edições por Ano/edicoes_2022.json")
+output_file_path = os.path.join(script_dir, f"Edições por Ano/edicoes_{year}.json")
 
 # Renomeia o arquivo JSON gerado pelo scrapy para o caminho desejado
-os.rename("Edições por Ano/edicoes_2022.json", output_file_path)
+os.rename(f"Edições por Ano/edicoes_{year}.json", output_file_path)
 
 # Verifica se a lista de dados foi carregada corretamente
-print("Execução bem-sucedida! O arquivo 'edicoes_2022.json' foi gerado.")
+print(f"Execução bem-sucedida! O arquivo 'edicoes_{year}.json' foi gerado.")
 
 # Caminho completo para o arquivo JSON gerado pelo scrapy
-json_file_path = os.path.join(script_dir, "Edições por Ano/edicoes_2022.json")
+json_file_path = os.path.join(script_dir, f"Edições por Ano/edicoes_{year}.json")
 
 # Chama a função url_Json com os dados lidos do arquivo JSON
 url_Json(json_file_path)
 print("Execução da função url_Json foi bem-sucedida!")
 
 # Pasta contendo os arquivos JSON
-pasta_diarios = os.path.join(script_dir, 'Diários em json/diarios_2022')
+pasta_diarios = os.path.join(script_dir, f"Diários em json/diarios_{year}")
 
 # Lista para armazenar os trechos filtrados
 trechos_filtrados = []
@@ -54,10 +59,17 @@ json_str = json_str.replace('[', '[\n ')
 # Adiciona uma quebra de linha no final
 json_str = json_str.replace(']', '\n]')
 
-# Salva os trechos filtrados em um novo arquivo JSON no mesmo diretório do script
-filtered_file_path = os.path.join(script_dir, 'Diários em json/diarios_2022/trechos_filtrados_2022.json')
+# Cria o caminho completo para a pasta "filtrados"
+filtered_directory = os.path.join(script_dir, "filtrados")
+
+# Verifica se o diretório "filtrados" existe, se não, cria
+if not os.path.exists(filtered_directory):
+    os.makedirs(filtered_directory)
+
+# Salva os trechos filtrados em um novo arquivo JSON na pasta "filtrados"
+filtered_file_path = os.path.join(filtered_directory, f"trechos_filtrados_{year}.json")
 
 # Salva os trechos filtrados em um novo arquivo JSON
 with open(filtered_file_path, 'w', encoding='utf-8') as output_file:
     output_file.write(json_str)
-print("Execução do filtro bem-sucedida! O arquivo 'trechos_filtrados_2022.json' foi gerado")
+print(f"Execução do filtro bem-sucedida! O arquivo 'trechos_filtrados_{year}.json' foi gerado")

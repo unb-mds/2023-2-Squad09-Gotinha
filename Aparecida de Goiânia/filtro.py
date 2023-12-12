7e6a7e4d81d79dcaabe068992fc964209d0b1020
Yan Lucas Souza Guimarães <ylsg0303@gmail.com>
2023-12-12 18:15:22 -0300

    Update filtro.py

--- Aparecida de Goiânia/filtro.py
+++ Aparecida de Goiânia/filtro.py
@@ -7,11 +7,15 @@ def extrair_trechos(arquivo_path):
     # Lista para armazenar os trechos filtrados
     trechos_filtrados = []
 
-    # Expressão regular para encontrar o padrão "EXTRATO DE CONTRATO Nº" até "VALOR:"
-    padrao = re.compile(r'EXTRATO\s+DE\s+CONTRATO\s+Nº\s*(\d+)/(\d+).*?(V ALOR:|VALOR:)\s*([\d,.]+)', re.DOTALL | re.IGNORECASE)
+    # Conjunto para armazenar trechos únicos
+    trechos_unicos = set()
+
+    # Expressão regular para encontrar o padrão "EXTRATO DE CONTRATO Nº" até "R$"
+    padrao = re.compile(r'EXTRATO\s+DE\s+CONTRATO\s+Nº\s*(\d+)/(\d+).*?R\$\s*([\d,.]+)|R\$\s*([\d,.]+)', re.DOTALL | re.IGNORECASE)
 
     # Expressão regular para identificar contratos relacionados à saúde
-    padrao_saude = re.compile(r'\b(?:municipal\s+de\s+saúde|município\s+de\s+saúde)\b', re.IGNORECASE)
+    padrao_saude = re.compile(r'\b(?:municipal\s+de\s+)?saúde\b', re.IGNORECASE)
+
 
     # Extrair a data do nome do arquivo
     nome_arquivo = os.path.basename(arquivo_path)
@@ -30,34 +34,46 @@ def extrair_trechos(arquivo_path):
             
             # Verifica se o contrato está relacionado à saúde
             if padrao_saude.search(trecho):
-                # Extrair o valor diretamente após "V ALOR:" ou "R$"
-                valor_match = re.search(r'(?:V ALOR:|R\$)\s*([\d,.]+)', trecho)
-                valor_str = valor_match.group(1).replace('.', '').replace(',', '')
-                
-                # Converter o valor para float e dividir por 100
-                valor = float(valor_str) / 100.0
+                # Extrair o valor diretamente após "R$"
+                valor_str = ocorrencia.group(3) or ocorrencia.group(4)
+                if valor_str is not None:
+                    valor_str = valor_str.replace('.', '').replace(',', '')
+                    
+                    # Verifica se o trecho já foi adicionado
+                    if trecho not in trechos_unicos:
+                        # Adiciona o trecho ao conjunto de trechos únicos
+                        trechos_unicos.add(trecho)
+                        
+                        # Converter o valor para float e dividir por 100
+                        valor = float(valor_str) / 100.0
 
-                trechos_filtrados.append({
-                    'Ano': ano,
-                    'Mês': mes,
-                    'Valor em R$': valor
-                })
+                        trechos_filtrados.append({
+                            'Ano': ano,
+                            'Mês': mes,
+                            'Valor em R$': valor
+                        })
             
             else:
-                # Se a expressão 'municipal de saúde' não foi encontrada antes do valor, procurar depois
+                # Se a expressão 'saúde' não foi encontrada antes do valor, procurar depois
                 trecho_posterior = texto[ocorrencia.end():ocorrencia.end() + 100]  # Considerando os próximos 100 caracteres
                 if padrao_saude.search(trecho_posterior):
-                    # Extrair o valor diretamente após "V ALOR:" ou "R$"
-                    valor_match = re.search(r'(?:V ALOR:|R\$)\s*([\d,.]+)', trecho)
-                    valor_str_posterior = valor_match.group(1).replace('.', '').replace(',', '')
-                    
-                    # Converter o valor para float e dividir por 100
-                    valor_posterior = float(valor_str_posterior) / 100.0
+                    # Extrair o valor diretamente após "R$"
+                    valor_match_posterior = re.search(r'R\$\s*([\d,.]+)', trecho_posterior, re.IGNORECASE)
+                    if valor_match_posterior and valor_match_posterior.group(1) is not None:
+                        valor_str_posterior = valor_match_posterior.group(1).replace('.', '').replace(',', '')
 
-                    trechos_filtrados.append({
-                        'Ano': ano,
-                        'Mês': mes,
-                        'Valor em R$': valor_posterior
-                    })
+                        # Verifica se o trecho já foi adicionado
+                        if trecho not in trechos_unicos:
+                            # Adiciona o trecho ao conjunto de trechos únicos
+                            trechos_unicos.add(trecho)
+
+                            # Converter o valor para float e dividir por 100
+                            valor_posterior = float(valor_str_posterior) / 100.0
+
+                            trechos_filtrados.append({
+                                'Ano': ano,
+                                'Mês': mes,
+                                'Valor em R$': valor_posterior
+                            })
         
     return trechos_filtrados
